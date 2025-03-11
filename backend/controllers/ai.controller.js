16f6ecd8525d8d68e9e5cf75adaa679039025ca9
surender singh <singhsurender28231@gmail.com>
2025-03-12 02:38:32 +0530

    vercel

--- backend/controllers/ai.controller.js
+++ backend/controllers/ai.controller.js
@@ -2,7 +2,7 @@ import {generateResponse} from '../services/ai.service.js';
 
 export const getResponse =  async (req, res) => {
     const {code} = req.body;
-
+try {
     if(!code) {
         res.status(400).send('error');
     }
@@ -10,4 +10,8 @@ export const getResponse =  async (req, res) => {
     const response = await generateResponse(code);
 
    res.send(response);
+} catch (error) {
+    res.status(500).json({ message: "Server error" });
+}
+   
 }
